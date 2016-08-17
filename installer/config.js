331a9cc7bd25989682d9888087e7afbf8c8b5fb2
Eric Martinez <emartinez@usgs.gov>
2016-08-16 18:15:14 -0600

    Fixed typos in edition string. Alphabetized.

--- installer/config.js
+++ installer/config.js
@@ -1,31 +1,34 @@
 var ftproot;
 
-ftproot = 'ftp://hazards.cr.usgs.gov/web/earthquake-hazard-tool';
+ftproot = '/web/earthquake-hazard-tool';
 
 module.exports = {
   configFile: __dirname + '/../conf/config.json',
+  ftpHost: 'hazards.cr.usgs.gov',
+  ftpPort: 21,
+  metadataFile: __dirname + '/../database/metadata.sql',
   nonInteractive: process.env.NON_INTERACTIVE === 'true',
   schemaFile: __dirname + '/../database/schema.sql',
   dataFiles: [
     {
-      edition: '2014R1',
+      edition: 'E2014R1',
       imt: 'SA0P2',
+      path: `${ftproot}/E2014R1_COUS0P05_SA0P2_760_Curves.tar.gz`,
       region: 'COUS0P05',
-      url: `${ftproot}/E2014R1_COUS0P05_SA0P2_760_Curves.tar.gz`,
       vs30: '760'
     },
     {
-      edition: '2014R1',
+      edition: 'E2014R1',
       imt: 'SA1P0',
+      path: `${ftproot}/E2014R1_COUS0P05_SA1P0_760_Curves.tar.gz`,
       region: 'COUS0P05',
-      url: `${ftproot}/E2014R1_COUS0P05_SA1P0_760_Curves.tar.gz`,
       vs30: '760'
     },
     {
-      edition: '2014R1',
+      edition: 'E2014R1',
       imt: 'PGA',
+      path: `${ftproot}/E2014R1_COUS0P05_PGA_760_Curves.tar.gz`,
       region: 'COUS0P05',
-      url: `${ftproot}/E2014R1_COUS0P05_PGA_760_Curves.tar.gz`,
       vs30: '760'
     }
   ]
